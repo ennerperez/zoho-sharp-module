f781f99014978eb1bbf5c0b56298cae0fe754b03
Enner Pérez <ennerperez@gmail.com>
2022-06-04 11:47:47 -0500

    Zoho Records
    .net6

---
+++ Records/Product.cs
@@ -0,0 +1,18 @@
+﻿#if NET6_0_OR_GREATER
+using System;
+
+namespace Zoho.Records
+{
+    public record Product(
+        string product_id,
+        string name,
+        string description,
+        string status,
+        string redirect_url,
+        string emails_ids,
+        int plan_count,
+        int addons_count,
+        DateTime created_time,
+        DateTime? updated_time);
+}
+#endif
