acde87c68c0c034ae101c57067779c757f92b9ac
Edgar Rivero <riveroedgar.jm@gmail.com>
2023-06-02 00:05:39 -0500

    Project by responsible

---
+++ Records/Project/CustomFieldProject.cs
@@ -0,0 +1,12 @@
+﻿#if NET6_0_OR_GREATER
+// ReSharper disable NotAccessedPositionalProperty.Global
+// ReSharper disable once CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable InconsistentNaming
+namespace Zoho.Records.Project
+{
+    public record CustomFieldProject(
+        string Responsible
+    );
+}
+#endif
